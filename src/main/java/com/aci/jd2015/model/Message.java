8526b6cad6dec9f2abc37cbddc7d776975388755
Denis <chempikd@yandex.ru>
2016-10-10 20:13:09 +0300

    It is the first commit

--- src/main/java/com/aci/jd2015/model/Message.java
+++ src/main/java/com/aci/jd2015/model/Message.java
@@ -6,6 +6,8 @@ public class Message implements Comparable<Message>{
 
 	private final Date datetime;
 	private final String message;
+	
+	//Vot eto daaaaa
 
 	public Message(Date datetime, String message) {
 		this.datetime = datetime;
